f5836391532f92ac28792e50fc7b5dcaa02302fc
zhangtongkai.sh <zhangtongkai.sh@bytedance.com>
2021-10-06 13:31:55 +0800

    first commit
    Change-Id: I7a6f3aa68d5d7b185db094f79609d4226e8a98bf

---
+++ init.el
@@ -0,0 +1,13 @@
+;;(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; Package Management
+;; -----------------------------------------------------------------
+(require 'init-packages)
+(require 'variables)
+(require 'init-org)
+(require 'org-download)
+
+
+
---
+++ lisp/variables.el
@@ -0,0 +1,6 @@
+(setenv "CLOUD" "/Users/bytedance/Library/Mobile Documents/com~apple~CloudDocs/Documents/org-mode")
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+(provide 'variables)
